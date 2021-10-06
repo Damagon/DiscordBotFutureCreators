2c0215c96b2b0e0dceebd6b9b6b4dd53fac9fda7
Damagon <30881380+Damagon@users.noreply.github.com>
2021-10-05 21:57:48 -0600

    Delete Folder directory

--- Folder/commands/fc.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    name: 'fc',
-    description: "command for future creators website!",
-    execute(message, args){
-        message.channel.send('Here is link to FC website: https://uafc.ca');
-    }
-}
--- Folder/commands/ttt.js
+++
@@ -1,8 +0,0 @@
-const TicTacToe = require('discord-tictactoe');
-const Discord = require('discord.js');
-const client = new Discord.Client();
-
-new TicTacToe({ language: 'en', command: '!ttt' })
-  .attach(client);
-
-client.login('ODU2NDcxMjExMjM5NTM4Njg4.YNBg-Q.zsPTXbDIRQ1Na1JeNNHhq7-c7y0');
